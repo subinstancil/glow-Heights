aff9bd8c619122455f72aadf1ab843a61eac60b7
subinstancil <subinstancil@gmail.com>
2020-07-04 08:44:39 +0530

    login screen update

--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin - Login</title>
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>assets/assets/images/favicon.png">
+	<title>Glow Heights</title>
 	<link href="<?php echo base_url();?>assets/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<style>
+		body  {
+		  background-image: url("<?php echo base_url();?>assets/assets/images/bg.jpeg");
+		  background-color: #cccccc;
+		}
+</style>
 </head>
 <body>
-	<div class="login col-md-4 mx-auto text-center">
-		<h1>Admin Login</h1>
+	<div class="login col-sm-12 text-center float-right">
 		<form method="post" action="<?php echo base_url('login/verify')?>">
 			<div class="form-group">
 				<input type="text" name="username" placeholder="Username" class="form-control">
@@ -16,9 +22,194 @@
 			</div>
 			<div class="form-group">
 				<input type="submit" name="submit" value="Login" class="btn btn-primary">
+				<input type="button" name="register" value="register" class="btn btn-default">
 			</div>
 			
 		</form>
 	</div>
 </body>
+</html> -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>assets/assets/images/favicon.png">
+    <title>Glow Heights : login</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url();?>assets/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="<?php echo base_url();?>assets/css/colors/default-dark.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="fix-header card-no-border fix-sidebar">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+  
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <!-- ============================================================== -->
+                <!-- Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html">
+                        <!-- Logo icon --><b>
+                            <img src="<?php echo base_url();?>assets/assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+                        </b>
+                        <!--End Logo icon -->
+                        <!-- Logo text -->
+                        <span>
+                            <img src="<?php echo base_url();?>assets/assets/images/logo-text.png" alt="homepage" class="dark-logo" />
+                        </span>
+                    </a>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-collapse">
+                    <!-- ============================================================== -->
+                    <!-- toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <ul class="navbar-nav mr-auto">
+                        <!-- This is  -->
+                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                    </ul>
+                    <!-- ============================================================== -->
+                    <!-- User profile and search -->
+                    <!-- ============================================================== -->
+                    
+                </div>
+            </nav>
+        </header>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper" style="margin-left: 0px; background-image: url('<?php echo base_url();?>assets/assets/images/bg.jpeg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #464646;" >
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+               
+                <div class="row">
+                    
+                    <div class="col-md-4 ml-auto fixed-bottom mb-5">
+                        <div class="card shadow-lg mb-5 rounded" style="-webkit-box-shadow: 0px 0px 31px 15px rgba(0,0,0,0.45);
+-moz-box-shadow: 0px 0px 31px 15px rgba(0,0,0,0.45);
+box-shadow: 0px 0px 31px 15px rgba(0,0,0,0.45);">
+                            <div class="card-body ">
+
+                            	 <div class="d-flex">
+                                    <div>
+                                        <h3 class="card-title mb-3"><span class="lstick"></span>Login </h3>
+                                    </div>
+                                 </div>
+
+                                <form class="form-horizontal form-material" method="post" action="<?php echo base_url('login/verify')?>">
+                                    <div class="form-group">
+                                       
+                                        <div class="col-md-12">
+                                            <input type="text" placeholder="User Name" class="form-control form-control-line" name="username" placeholder="Username">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        
+                                        <div class="col-md-12">
+                                            <input type="password" placeholder="Password" class="form-control form-control-line" name="password" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                    	<center><div class="col-md-8">
+                                    	
+                                       
+										<input type="button" name="register" value="register" class="btn btn-default">
+										 <input type="submit" name="submit" value="Login" class="btn btn-success ml-3">
+                                        </div></center>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- Row -->
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
+                <!-- ============================================================== -->
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer">
+                © 2020 Glow Heights
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="<?php echo base_url();?>assets/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?php echo base_url();?>assets/assets/plugins/bootstrap/js/popper.min.js"></script>
+    <script src="<?php echo base_url();?>assets/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/perfect-scrollbar.jquery.min.js"></script>
+    <!--Wave Effects -->
+    <script src="<?php echo base_url();?>assets/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="<?php echo base_url();?>assets/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/custom.min.js"></script>
+</body>
+
 </html>
